4481b51aca870d13476d965c77a97c23e0d1c9e9
Wansoo Cho <14wcho@gmail.com>
2025-12-16 00:08:40 -0500

    added Java springboot and VSCode, removed open api key

---
+++ springboot-chatgpt/springboot-chatgpt/src/main/java/com/springboot/springboot/chatgpt/controller/ChatGPTController.java
@@ -0,0 +1,23 @@
+package com.springboot.springboot.chatgpt.controller;
+
+
+import com.springboot.springboot.chatgpt.dto.PromptRequest;
+import com.springboot.springboot.chatgpt.service.ChatGPTService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/chat")
+public class ChatGPTController {
+
+    private final ChatGPTService chatGPTService;
+
+    public ChatGPTController(ChatGPTService chatGPTService) {
+        this.chatGPTService = chatGPTService;
+    }
+
+    @PostMapping
+    public String chat(@RequestBody PromptRequest promptRequest) {
+        return chatGPTService.getChatResponse(promptRequest);
+    }
+}
---
+++ springboot-chatgpt/springboot-chatgpt/src/main/java/com/springboot/springboot/chatgpt/dto/PromptRequest.java
@@ -0,0 +1,4 @@
+package com.springboot.springboot.chatgpt.dto;
+
+public record PromptRequest(String prompt) {
+}
